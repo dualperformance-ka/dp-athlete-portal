fe130cf23f8e040a035a916d4ea472c36924bab2
dualperformance-ka <dualperformanceka@gmail.com>
2026-07-11 05:57:42 +0930

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'dp-athlete-v23';
+const CACHE_NAME = 'dp-athlete-v24';
 const APP_SHELL = [
-  '/index.html', '/styles.css?v=15', '/config.js',
+  '/index.html', '/styles.css?v=16', '/config.js',
   '/app.js', '/login.js', '/icons.css?v=1',
   '/dual_performance_one_line_filled_logo_black_preview.png',
   '/dp_baby_blue_transparent_512x512.png'
